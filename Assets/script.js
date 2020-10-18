723926c9f6a742d2d07826ea780338466a3f69ee
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-18 17:07:42 -0500

    realized that one-call for the weather API doesn't accept city ID like I thought it would, so now everything is broken since I don't have all the data I need. Working on fixing it.

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,33 @@
 $(document).ready(function () {
+  var URL_KEY = "7ecfb723466707858711a0159dd5300d";
   var cityString = localStorage.getItem("Cities");
   var cityID = [];
   if (cityString) {
     cityID = JSON.parse(cityString);
   }
   updateCities(cityID);
-  function buildQueryURL() {
+  function buildCityURL() {
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=";
-    var URL_Key = "7ecfb723466707858711a0159dd5300d";
     var cityQuery = "&q=" + $("#user-city").val().trim();
-    return queryURL + URL_Key + cityQuery;
+    return queryURL + URL_KEY + cityQuery;
+  }
+  function buildCoordURL(coordObj) {
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/onecall?units=imperial&appid=";
+    var Citylat = "&lat=" + coordObj.lat;
+    var Citylon = "&lon=" + coordObj.lon;
+    return queryURL + URL_KEY + Citylat + Citylon;
   }
 
   function updateCities(array) {
-    $("#favorite-cities").empty();
+    $("#favorite-cities").empty().text();
     $(array).each(function (idx, elem) {
-      var thisCity = $("<button>").text(elem.name).val(elem.id);
+      var thisCity = $("<button>").text(elem.name).val(elem.name);
       if (idx === array.length - 1) {
-        thisCity.attr("class", "btn btn-success btn-lg btn-block active");
+        thisCity.attr("class", "btn btn-success btn-lg btn-block");
+        // how do I make this button stop being active if something else is clicked?
+        // .toggleClass("active");
       } else {
         thisCity.attr("class", "btn btn-lg btn-block btn-success");
       }
@@ -27,15 +36,51 @@ $(document).ready(function () {
     localStorage.setItem("Cities", JSON.stringify(array));
   }
 
+  function renderWeather(coordObj) {
+    var queryURL = buildCoordURL(coordObj);
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (forecastData) {
+      console.log(forecastData);
+      $("#current-city").text(forecastData.name);
+      var currentDate = moment().format("MMMM Do YYYY");
+      var weatherIcon =
+        "http://openweathermap.org/img/wn/" +
+        forecastData.weather[0].icon +
+        "@2x.png";
+      $("#current-title").text("Weather Conditions on " + currentDate);
+      $("#current-conditions").html(
+        '<img src="' +
+          weatherIcon +
+          ' alt="Current Weather Icon"/> <p>Temperature: ' +
+          forecastData.main.temp +
+          "°F</p> <p>Humidity: " +
+          forecastData.main.humidity +
+          "%RH</p> <p>Wind Speed: " +
+          forecastData.wind.speed +
+          " MPH</p>"
+      );
+      var $index = $("<p>").text("UV Index: ");
+      var UV = $index.appendTo("#current-conditions");
+    });
+    $("#current-card").attr("class", "card");
+    $("#current-city").attr("class", "card-header");
+  }
+
   $("#new-city").on("submit", function (event) {
     event.preventDefault();
-    var queryURL = buildQueryURL();
+    var queryURL = buildCityURL();
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (cityData) {
       var tester = true;
-      var newCity = { name: cityData.name, id: cityData.id };
+      var newCity = {
+        name: cityData.name,
+        lat: cityData.coord.lat,
+        lon: cityData.coord.lon,
+      };
       for (var i = 0; i < cityID.length; i++) {
         if (cityID[i].name === newCity.name) {
           tester = false;
@@ -47,6 +92,7 @@ $(document).ready(function () {
         cityID.push(newCity);
         updateCities(cityID);
       }
+      renderWeather(newCity);
       $("#user-city").val("");
     });
   });
