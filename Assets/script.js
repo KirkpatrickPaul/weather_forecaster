5bb4e83d2343cf896a9a42323d129fbc045a2914
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-18 12:30:29 -0500

    data now persists in local storage and gets called when the pae is loaded.

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,47 @@
-function buildQueryURL() {
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=";
-  var URL_Key = "7ecfb723466707858711a0159dd5300d";
-  var cityQuery = "&q=" + $("#user-city").val().trim();
-  return queryURL + URL_Key + cityQuery;
-}
-// console.log("api.openweathermap.org/data/2.5/weather?units=imperial&appid=");
+$(document).ready(function () {
+  var cityString = localStorage.getItem("Cities");
+  var cityID = [];
+  if (cityString) {
+    cityID = JSON.parse(cityString);
+  }
+  updateCities(cityID);
+  function buildQueryURL() {
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=";
+    var URL_Key = "7ecfb723466707858711a0159dd5300d";
+    var cityQuery = "&q=" + $("#user-city").val().trim();
+    return queryURL + URL_Key + cityQuery;
+  }
 
-$("#new-city").on("submit", function (event) {
-  event.preventDefault();
-  var queryURL = buildQueryURL();
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (cityData) {
-    console.log(cityData);
+  function updateCities(array) {
+    $("#favorite-cities").empty();
+    $(cityID).each(function (idx, elem) {
+      var thisCity = $("<button>").text(array[idx].name).val(array[idx].id);
+      if (idx === cityID.length - 1) {
+        thisCity.attr("class", "btn btn-success btn-lg btn-block active");
+      } else {
+        thisCity.attr("class", "btn btn-lg btn-block btn-success");
+      }
+      thisCity.appendTo("#favorite-cities");
+    });
+    localStorage.setItem("Cities", JSON.stringify(cityID));
+  }
+
+  $("#new-city").on("submit", function (event) {
+    event.preventDefault();
+    var queryURL = buildQueryURL();
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (cityData) {
+      newCity = { name: cityData.name, id: cityData.id };
+      console.log(newCity);
+      //not sure why this if statement isn't working like I want it to.
+      if (!cityID.includes(newCity)) {
+        cityID.push(newCity);
+      }
+      updateCities(cityID);
+      $("#user-city").val("");
+    });
   });
 });
