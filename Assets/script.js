dcdd40cb016072d71873ea9ec9da37f6b73bd11d
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-17 22:08:57 -0500

    built ajax function and .on(submit) for form id. Added id to HTML for form.

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+function buildQueryURL() {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=";
+  var URL_Key = "7ecfb723466707858711a0159dd5300d";
+  var cityQuery = "&q=" + $("#user-city").val().trim();
+  return queryURL + URL_Key + cityQuery;
+}
+// console.log("api.openweathermap.org/data/2.5/weather?units=imperial&appid=");
+
+$("#new-city").on("submit", function (event) {
+  event.preventDefault();
+  var queryURL = buildQueryURL();
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(cityData);
+});
